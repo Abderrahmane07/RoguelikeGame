bcaec6eeaddef24f6e4f37492c04eff532bcac75
Abderrahmane07 <75677650+Abderrahmane07@users.noreply.github.com>
2021-04-27 05:21:28 +0200

    Ajout des connections avec les tunnels

--- Rogue/ConsoleApp1/Systems/MapGenerator.cs
+++ Rogue/ConsoleApp1/Systems/MapGenerator.cs
@@ -55,6 +55,30 @@ namespace ConsoleApp1.Systems
                 {
                     _map.Rooms.Add(newRoom);
                 }
+
+                // La paartie pour creer les connections entre les differentes rooms
+                // Iterer a travers chaque room generee, en commencant par la room 1 au lieu de 0
+                for (int rok=1; rok < _map.Rooms.Count; rok++)
+                {
+                    // commecons par trouver le centre de la room r et de celle qui la precede
+                    int previousRoomCenterX = _map.Rooms[rok - 1].Center.X;
+                    int previousRoomCenterY = _map.Rooms[rok - 1].Center.Y;
+                    int currentRoomCenterX = _map.Rooms[rok].Center.X;
+                    int currentRoomCenterY = _map.Rooms[rok].Center.Y;
+
+                    // 50% de chance de choisir un des deux choix
+                    if (Game.Random.Next(1, 2) == 1)
+                    {
+                        CreateHorizontalTunnel(previousRoomCenterX, currentRoomCenterX, previousRoomCenterY);
+                        CreateVerticalTunnel(previousRoomCenterY, currentRoomCenterY, previousRoomCenterX);
+                    }
+                    else
+                    {
+                        CreateVerticalTunnel(previousRoomCenterY, currentRoomCenterY, previousRoomCenterX);
+                        CreateHorizontalTunnel(previousRoomCenterX, currentRoomCenterX, previousRoomCenterY);
+                    }
+
+                }
             }
             foreach(Rectangle room in _map.Rooms)
             {
@@ -80,7 +104,7 @@ namespace ConsoleApp1.Systems
         {
             for (int y = Math.Min(yStart, yEnd); y <= Math.Max(yStart, yEnd); y++)
             {
-                _map.SetCellProperties(y, xPosition, true, true);
+                _map.SetCellProperties(xPosition, y, true, true);
             }
         }
 
@@ -105,7 +129,7 @@ namespace ConsoleApp1.Systems
             {
                 for(int y=room.Top+1; y<room.Bottom; y++)
                 {
-                    _map.SetCellProperties(x, y, true, true, true);
+                    _map.SetCellProperties(x, y, true, true, false);
                 }
             }
         }
